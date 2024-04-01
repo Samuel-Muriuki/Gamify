277abf884d6fcbc91fdc7a7d94bde035918ed8ef
Samuel Muriuki <sammkimberly@gmail.com>
2024-04-01 06:45:07 +0300

    ✨feat[Handling Events]: -Executing the handleClick fn in response to a click event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,22 +1,35 @@
-function ListGroup() {
-	const items = [
-		"New York",
-		"Nairobi",
-		"Hellsinki",
-		"Paris",
-		"Tokyo",
-		"London",
-	]
+import { MouseEvent } from "react";
 
-	return (
-		<>
-			<h1>List</h1>
-			<ul className="list-group">
-				{items.length === 0 && <p>No item found</p>}
-				{items.map(item => <li key={item} className="list-group-item">{item}</li>)}
-			</ul>
-		</>
-	);
+function ListGroup() {
+    const items = [
+        "New York",
+        "Nairobi",
+        "Hellsinki",
+        "Paris",
+        "Tokyo",
+        "London",
+    ];
+
+    // Event handler
+    const handleClick = (event: MouseEvent) => (console.log(event));
+
+    return (
+        <>
+            <h1>List</h1>
+            <ul className="list-group">
+                {items.length === 0 && <p>No item found</p>}
+                {items.map((item, index) => (
+                    <li
+                        key={item}
+                        className="list-group-item"
+                        onClick={handleClick} // Just passing a reference, not calling the fn. Calling the fn will be done later at runtime
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default ListGroup;
