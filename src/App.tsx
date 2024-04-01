1cae9de486095d24ab0cd7eccdf4ced8c3676ae6
Samuel Muriuki <sammkimberly@gmail.com>
2024-04-01 12:02:57 +0300

    ✨feat[Props]: -Properties are arguments passed from one react component to another

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>
+    const items = [
+        "New York",
+        "Nairobi",
+        "Hellsinki",
+        "Paris",
+        "Tokyo",
+        "London",
+    ];
+
+    return (
+        <div>
+            <ListGroup items={items} header={"Cities"} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,28 +1,29 @@
 import { useState } from "react";
 
-function ListGroup() {
-    const items = [
-        "New York",
-        "Nairobi",
-        "Hellsinki",
-        "Paris",
-        "Tokyo",
-        "London",
-    ];
+interface Props {
+    items: string[];
+    header: string;
+}
 
-    // Hook
+function ListGroup({ items, header }: Props) {
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
     return (
         <>
-            <h1>List</h1>
+            <h1>{header}</h1>
             <ul className="list-group">
                 {items.length === 0 && <p>No item found</p>}
                 {items.map((item, index) => (
                     <li
                         key={item}
-                        className={selectedIndex === index ? `list-group-item active` : `list-group-item`}
-                        onClick={() => { setSelectedIndex(index) }}
+                        className={
+                            selectedIndex === index
+                                ? `list-group-item active`
+                                : `list-group-item`
+                        }
+                        onClick={() => {
+                            setSelectedIndex(index);
+                        }}
                     >
                         {item}
                     </li>
