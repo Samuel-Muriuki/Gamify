5b9527eb4c5c1d20e85a962573269098369109cc
Samuel Muriuki <sammkimberly@gmail.com>
2024-04-02 19:42:49 +0300

    📝memo[Sate Structure]: -Best Practices when using the state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import Like from "./components/Like";
+import { useState } from "react";
 
 function App() {
+
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName, setLastName] = useState(""); 
+    
+
+    const [person, setPerson] = useState({ // Group related variables inside  an object
+        firstName: "", // Avoid deeply nested structures
+        lastName: "",
+    })
+
+    /*  Avoid redundant state variables
+        No need to use the state hook to declare another variable 'fullName'
+    */
     return (
         <div>
-            <Like onClick={() => console.log("Clicked")} />
         </div>
     );
 }
