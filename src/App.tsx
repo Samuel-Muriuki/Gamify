b7ec75471e21a077a9e783a673297919f907ad31
Samuel Muriuki <sammkimberly@gmail.com>
2024-04-02 21:08:46 +0300

    📝memo[Pure Functions]: -Keep changes out of the render phase to keep components pure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
+import Message from "./Message";
 
 function App() {
-
-    // const [firstName, setFirstName] = useState("");
-    // const [lastName, setLastName] = useState(""); 
-    
-
-    const [person, setPerson] = useState({ // Group related variables inside  an object
-        firstName: "", // Avoid deeply nested structures
-        lastName: "",
-    })
-
-    /*  Avoid redundant state variables
-        No need to use the state hook to declare another variable 'fullName'
-    */
     return (
         <div>
+            <Message />
+            <Message />
+            <Message />
         </div>
     );
 }
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,7 @@
-function Message() {
-    return <h1>Hello World</h1>
+let count = 0;
+const Message = () => {
+    count++; // This makes the component impure as the count variable is being updated as part of rendering the message component
+    return <h1>Message {count}</h1>
 }
 
 export default Message;
