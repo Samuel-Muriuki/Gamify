521ed2bbfdbaf129daf117981131c7cf5bee7e6a
Samuel Muriuki <sammkimberly@gmail.com>
2024-04-02 21:15:18 +0300

    📝memo[Strict Mode]: -Used to highligh potential problems in development such as impure components

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,7 @@
 let count = 0;
 const Message = () => {
-    count++; // This makes the component impure as the count variable is being updated as part of rendering the message component
+    console.log("Message called: ", count);
+    count++;
     return <h1>Message {count}</h1>
 }
 
