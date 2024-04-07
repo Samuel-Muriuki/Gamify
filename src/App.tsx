7a9ec8e47568232ff1478c211f6011fb711861e3
Samuel Muriuki <sammkimberly@gmail.com>
2024-04-07 21:11:22 +0300

    ✨feat[Updating with immer]: -Using the immer library to simplify immutable object updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { produce } from "immer";
 
 function App() {
     const [bugs, setBugs] = useState([
@@ -7,14 +8,16 @@ function App() {
     ])
 
     const handleClick = () => {
-        console.log(bugs);
-        setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true} : bug));
-        console.log(bugs);
-        
+        // setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true} : bug));
+        setBugs(produce(draft => {
+            const bug = draft.find(bug => bug.id === 1);
+            if (bug) bug.fixed = true;
+        }))
     }
     
     return (
         <div>
+            {bugs.map(bug => <p key={bug.id}>{bug.title} {bug.fixed ? "Fixed" : "New"}</p>)}
             <button onClick={handleClick}>Click Me</button>
         </div>
     );
