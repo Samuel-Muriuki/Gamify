3cfeab35c9d135fdfde14099c4355b95ca9f79a0
Samuel Muriuki <sammkimberly@gmail.com>
2024-04-07 16:59:59 +0300

    ✨feat[Updating Nested Objects]: -Using the spread syntax to copy the object properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import { useState } from "react";
 
 function App() {
-    const [drink, setDrink] = useState({
-        title: "Americano",
-        price: 5,
+    const [customer, setCustomer] = useState({
+        name: "John",
+        address: {
+            city: "San Francisco",
+            zipCode: 94111,
+        },
     });
 
     const handleClick = () => {
-        // const newDrink = {
-        //     title : drink.title,
-        //     price: 6,
-        // }
-        // setDrink(newDrink);
-        setDrink({ ...drink, price: 6 }) // This one line replaces the above commented code. Using the spread syntax to iterate through the obj and have access to all the properties
-    }
+        setCustomer({
+            ...customer,
+            address: { ...customer.address, zipCode: 94112 },
+        });
+    };
 
     return (
         <div>
-            {drink.price}
             <button onClick={handleClick}>Click Me</button>
         </div>
     );
