48ddc501780d133ca5f0d68dc27b18d7f551f8e6
Samuel Muriuki <sammkimberly@gmail.com>
2024-04-07 16:53:11 +0300

    ✨feat[Updating Objects]: -Using the spread syntax to copy the object properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import Message from "./Message";
+import { useState } from "react";
 
 function App() {
+    const [drink, setDrink] = useState({
+        title: "Americano",
+        price: 5,
+    });
+
+    const handleClick = () => {
+        // const newDrink = {
+        //     title : drink.title,
+        //     price: 6,
+        // }
+        // setDrink(newDrink);
+        setDrink({ ...drink, price: 6 }) // This one line replaces the above commented code. Using the spread syntax to iterate through the obj and have access to all the properties
+    }
+
     return (
         <div>
-            <Message />
-            <Message />
-            <Message />
+            {drink.price}
+            <button onClick={handleClick}>Click Me</button>
         </div>
     );
 }
