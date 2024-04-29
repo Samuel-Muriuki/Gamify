8086db445374d4d27f0860ac84b7c390c6c88888
Samuel Muriuki <sammkimberly@gmail.com>
2024-04-29 20:52:46 +0300

    ✨feat[Exercise on Updating State]: -Updating Nested Objects and Arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
 import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
 
 function App() {
-    const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+    const [game, setGame] = useState({
+        id: 1,
+        player: {
+            name: "John",
+        },
+    });
+
+    const [pizza, setPizza] = useState({
+        name: "Spicy Pepperoni",
+        toppings: ["Mushroom"],
+    })
+
+    const [cart, setCart] = useState({
+        discount: .1,
+        items: [
+            {id: 1, title: "Product 1", quantity: 1},
+            {id: 2, title: "Product 2", quantity: 1},
+        ]
+    })
+
+    const handleClick = () => {
+        console.log("game: ", game);
+        console.log("pizza: ", pizza);
+        console.log("cart: ", cart);
+        
+        setGame({ ...game, player: { ...game.player, name: "Samm" } });
+
+        setPizza({ ...pizza, toppings: [ ...pizza.toppings, "Cheese" ] });
+
+        setCart({ ...cart, items: cart.items.map(item => item.id === 1 ? { ...item, quantity:item.quantity + 1 } : item) })
+
+        console.log("Game: ", game);
+        console.log("Pizza: ", pizza);
+        console.log("Cart: ", cart);
+    }
     
     return (
         <div>
-            <NavBar cartItemsCount={cartItems.length} />
-            <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+            <button onClick={handleClick}>Click Me</button>
         </div>
     );
 }
